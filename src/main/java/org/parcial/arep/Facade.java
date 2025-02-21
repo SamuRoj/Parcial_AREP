70ad251df4839bd2d015e5c414d8bae506d8691e
unknown <samuel.rojas-y@labinfo.is.escuelaing.edu.co>
2025-02-21 10:48:23 -0500

    Servers

--- src/main/java/org/parcial/arep/Facade.java
+++ src/main/java/org/parcial/arep/Facade.java
@@ -1,4 +1,119 @@
 package org.parcial.arep;
 
+import java.net.*;
+import java.io.*;
+
 public class Facade {
+
+    private static boolean isRunning = true;
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "http://localhost:45000";
+
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        while (isRunning) {
+
+            // Starting up the facade server
+            Socket clientSocket = null;
+            try {
+                System.out.println("Facade listo para recibir...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            // Reading the request received
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            String firstLine = "";
+            boolean isFirst = true;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Facade line: " + inputLine);
+                if(isFirst){
+                    firstLine = inputLine;
+                    isFirst = false;
+                }
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            String method = firstLine.split(" ")[0];
+            String path = firstLine.split(" ")[1];
+
+            if(path.startsWith("/cliente")){
+                outputLine = readIndex();
+            }
+            else if(path.startsWith("/consulta")){
+
+            }
+            else{
+                outputLine = "HTTP/1.1 404 NOT FOUND\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "\r\n"
+                        + "<!DOCTYPE html>\n"
+                        + "<html>\n"
+                        + "<head>\n"
+                        + "<meta charset=\"UTF-8\">\n"
+                        + "<title>Error Page</title>\n"
+                        + "</head>\n"
+                        + "<body>\n"
+                        + "<h1>El recurso solicitado no fue encontrado</h1>\n"
+                        + "</body>\n"
+                        + "</html>\n";
+            }
+
+            // Sending the answer of the request
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    private static String readIndex() {
+
+    }
+
+    public static void httpServerRequest(String method, String path) throws IOException {
+        // Connection to HttpServer class
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod(method);
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println(method + " Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response);
+        } else {
+            System.out.println(method + " request not worked");
+        }
+        System.out.println(method + " DONE");
+    }
 }
+
--- src/main/java/org/parcial/arep/HttpServer.java
+++ src/main/java/org/parcial/arep/HttpServer.java
@@ -1,4 +1,59 @@
 package org.parcial.arep;
 
+import java.net.*;
+import java.io.*;
+
 public class HttpServer {
+
+    private static boolean isRunning = true;
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(45000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 45000.");
+            System.exit(1);
+        }
+
+        while (isRunning) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("HttpServer listo para recibir...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            // Reading the request received
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("HttpServer line: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            // Output of the request
+            outputLine = "<!DOCTYPE html>" +
+                            "<html>" +
+                            "<head>" +
+                            "<meta charset=\"UTF-8\">" +
+                            "<title>Title of the document</title>\n" +
+                            "</head>" +
+                            "<body>" +
+                            "<h1>Mi propio mensaje</h1>" +
+                            "</body>" +
+                            "</html>";
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
 }
